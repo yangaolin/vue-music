855a70b8cfb13eeb641df6106e56b8bc8e080458
aolin yang <aolin.yang@zhaopin.com.cn>
2017-11-14 21:05:50 +0800

    歌手页面大部分完成

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="singer">
-    
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+  import ListView from 'base/listview/listview'
 
   const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
   export default {
     data() {
       return{
@@ -22,8 +25,7 @@
       _getSingerList() {
         getSingerList().then((res) => {
           if(res.code === ERR_OK){
-            this.singers = res.data.list
-            console.log(this.singers)
+            this.singers = this._normalizeSinger(res.data.list)
           }
         })
       },
@@ -34,10 +36,44 @@
             items: []
           }
         }
-        list.forEach(() => {
-
+        list.forEach((item,index) => {
+          if(index<HOT_SINGER_LEN){
+            map.hot.items.push(new Singer({
+              id:item.Fsinger_mid,
+              name:item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if(!map[key]){
+            map[key] = {
+              title:key,
+              items:[]
+            }
+          }
+          map[key].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))  
+        })       
+        // 为了得到有序列表   我们需要处理 map
+        let hot = []
+        let ret = []
+        for (let key in map){
+          let val = map[key]
+          if(val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if(val.title ==HOT_NAME){
+            hot.push(val)
+          }
+        }
+        ret.sort((a,b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
         })
+        return hot.concat(ret)
       }
+    },
+    components: {
+      ListView
     }
   }
 </script>
