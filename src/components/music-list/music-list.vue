616248cea4cf17c68c8ae049065f12a29d0a40a7
yangaolin <973956394@qq.com>
2018-01-31 00:46:28 +0800

    music-list组件开发2

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -3,15 +3,49 @@
     <div class="back">
       <i class="icon-back"></i>
     </div>
-    <h1 class="title"></h1>
-    <div class="bg-image">
+    <h1 class="title" v-html="title"></h1>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter"></div>
     </div>
+    <scroll :data="songs" class="list" ref="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  
+  import Scroll from 'base/scroll/scroll'
+  import SongList from 'base/song-list/song-list'
+  export default {
+    props: {
+      bgImage: {
+        type: String,
+        default: ''
+      },
+      songs: {
+        type: Array,
+        default: []
+      },
+      title: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      bgStyle() {
+        return `background-image: url(${this.bgImage})`
+      }
+    },
+    mounted() {
+      this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+    },
+    components: {
+      Scroll,
+      SongList
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
